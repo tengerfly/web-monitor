cd40ca0c991eb7ad768a39df373a36665b0de0c1
tenger <15136388892@163.com>
2025-03-02 15:32:08 +0800

    chore:  init

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,36 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettierPlugin from 'eslint-plugin-prettier'; // Prettier 插件
+import prettierConfig from 'eslint-config-prettier'; // 关闭冲突规则
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  // eslintRecommended,
   {
-    extends: [
-      'eslint:recommended',
-      "plugin:prettier/recommended", // 等价于同时启用 eslint-plugin-prettier + eslint-config-prettie
+    ignores: [
+      '**/dist/**',
+      '**/node_modules/**',
+      './.eslintrc.js',
+      './.prettierrc.js',
+      './.gitignore',
     ],
+    plugins: {
+      prettier: prettierPlugin, // 注册 Prettier 插件
+    },
     rules: {
-      'prettier/prettier': 'error', // 将 Prettier 错误视为 ESLint 错误
-        eqeqeq: "off",
-        "no-unused-vars": "error",
-        "prefer-const": ["error", { "ignoreReadBeforeAssign": true }]
-    }
+      ...prettierConfig.rules, // 关闭冲突的 ESLint 规则
+      'prettier/prettier': 'error', // 启用 Prettier 规则
+    },
+  },
+  // 4. 自定义规则
+  {
+    rules: {
+      'no-console': 'error',
+    },
   },
 ];
